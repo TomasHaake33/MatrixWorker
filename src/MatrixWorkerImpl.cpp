3eaec4e0825987351c108b17a5abd35088641a2f
Akhmat Kuchinaev <akhmat.kuchinaev@axxonsoft.dev>
2023-11-22 15:37:15 +0300

    Remove threads

--- src/MatrixWorkerImpl.cpp
+++ src/MatrixWorkerImpl.cpp
@@ -36,94 +36,29 @@ void CheckMatrix(const Matrix& matrix)
 } //namespace
 
 /**
- * \brief Transpose a part of matrix bounded by rows (naive)
- * 
- * \param destMatrix destination matrix
- * \param srcMatrix source matrix
- * \param beginRow row from thich the part starts
- * \param endRow row at which the part ends
- * \returntransposed part of the matrix
- */
-void MatrixWorker::PartialTranspose(Matrix& destMatrix, const Matrix& srcMatrix, const unsigned beginRow, const unsigned endRow)
-{
-    for (unsigned i = beginRow; i < endRow; ++i)
-    {
-        for (unsigned j = 0; j < srcMatrix.width; ++j)
-        {
-            destMatrix.data[j * destMatrix.width + i] = srcMatrix.data[i * srcMatrix.width + j];
-        }
-    }
-}
-
-/**
- * \brief Parallel matrix transposition
- * 
- * \param matrix
- * \param numThreads number of threads
- * \return transposed matrix
- */
-Matrix MatrixWorker::AsyncTranspose(Matrix& matrix, int numThreads)
-{
-    if (numThreads == 0)
-    {
-        throw(std::runtime_error("numThreads is zero"));
-    }
-
-    CheckMatrix(matrix);
-    Matrix matrixCopy(matrix); //matrix copy. Write the result in here, read from original
-    
-    auto rowStep = matrix.height / numThreads; //step in rows
-    if (rowStep == 0)
-    {
-        std::cout << "numThreads is too large for the current matrix. Attempting to run in a single thread" << std::endl;
-        numThreads = 1;
-        rowStep = matrix.height;
-    }
-
-    std::swap(matrixCopy.height, matrixCopy.width);
-
-    std::vector<std::thread> threads; //threads which transpose matrix parts 
-    unsigned beginRow = 0;
-    //numThreads - 1 threads will work on equal parts, the remainder is for the last thread
-    for (auto i = 0; i < numThreads - 1; ++i)
-    {
-        const auto endRow = beginRow + rowStep;
-        threads.emplace_back(std::thread(
-            &MatrixWorker::PartialTranspose, 
-            this,
-            std::ref(matrixCopy),
-            std::ref(matrix),
-            beginRow,
-            endRow));
-        beginRow += rowStep;
-    }
-    threads.emplace_back(std::thread(
-        &MatrixWorker::PartialTranspose,
-        this,
-        std::ref(matrixCopy),
-        std::ref(matrix), 
-        beginRow, 
-        matrix.height));
-
-    //waiting for threads to end
-    for (auto& thread : threads)
-    {
-        thread.join();
-    }
-
-    return matrixCopy;
-}
-
-/**
- * \brief Delayed AsyncTranspose execution
+ * \brief Async transposition of matrix
  * 
  * \param matrix
  * \return future with transposed matrix
  */
 std::future<Matrix> MatrixWorker::AsyncProcess(Matrix matrix)
 {
-    return std::async(std::launch::deferred, [=]() mutable {
-        return AsyncTranspose(matrix);
+    //Threads are not created on every call, but only if they are allowed to
+    return std::async([=]() mutable {
+        CheckMatrix(matrix);
+
+        Matrix matrixCopy = matrix;
+        std::swap(matrixCopy.height, matrixCopy.width);
+
+        for (unsigned i = 0; i < matrix.height; ++i)
+        {
+            for (unsigned j = 0; j < matrix.width; ++j)
+            {
+                matrixCopy.data[j * matrix.height + i] = matrix.data[i * matrix.width + j];
+            }
+        }
+
+        return matrixCopy;
     });
 }
 
--- src/MatrixWorkerImpl.h
+++ src/MatrixWorkerImpl.h
@@ -3,16 +3,12 @@
 
 #include <MatrixWorker/MatrixWorkerLib.h>
 
-constexpr int NUM_THREADS = 2; //number of threads for transposition. Must be <= matrix.height
-
 class MatrixWorker : public WorkerInterface
 {
 public:
     MatrixWorker() = default;
     ~MatrixWorker() = default;
 
-    Matrix AsyncTranspose(Matrix& matrix, int numThreads = NUM_THREADS);
-    void PartialTranspose(Matrix& destMatrix, const Matrix& srcMatrix, const unsigned beginRow, const unsigned endRow);
     std::future<Matrix> AsyncProcess(Matrix matrix) override;
 };
 
